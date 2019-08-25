4d00eea773707ab1f710e0fc49598b7b29d48995
unknown <dantecypress@gmail.com>
2019-08-24 18:05:32 -0700

    added API

---
+++ scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = "LLBfAXFnCUyFKpDpkAI2MusoDmLr3VPB";
+
+const getCity = async city => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// getCity("San Diego").then(data =>
+//   console.log(data).catch(err => {
+//     console.log(err);
+//   })
+// );
+
+//weather info
+const getWeather = async id => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  console.log(data);
+};
+
+getWeather("347628");
