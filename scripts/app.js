f2eaf0edbcbd9e9bf24698c1db7ccf3c7d381f24
unknown <dantecypress@gmail.com>
2019-08-25 15:27:56 -0700

    added icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = data => {
   const cityDets = data.cityDetails;
@@ -8,6 +10,8 @@ const updateUI = data => {
 
   //update details template
   details.innerHTML = ` 
+    <button id="fButton" type="button" class="btn btn-secondary">F</button>
+    <button id="cButton" type="button" class="btn btn-secondary">C</button>
     <h5 class="my-3">${cityDets.EnglishName}</h5>
     <div class="my-3">${weather.WeatherText}</div>
     <div class="display-4 my-4">
@@ -15,12 +19,31 @@ const updateUI = data => {
       <span>&deg;</span>
     </div>`;
 
+  // details.addEventListener("click", e => {
+  //   e.preventDefault();
+  //   newFloat = weather.Temperature.Metric.Value * 1.8 + 32;
+  //   console.log(newFloat);
+  //   finalFloat = newFloat.toFixed(1);
+
+  //   details.innerHTML = `<button id="fButton" type="button" class="btn btn-secondary">F</button>
+  //   <button id="cButton" type="button" class="btn btn-secondary">C</button>
+  //   <h5 class="my-3">${cityDets.EnglishName}</h5>
+  //   <div class="my-3">${weather.WeatherText}</div>
+  //   <div class="display-4 my-4">`;
+  //   if (details.classList.contains("fButton")) {
+  //     `<span>${finalFloat}</span>`;
+  //   }
+  //   console.log("hello");
+  // });
+
   //remove d-none if present
 
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
 };
+
+//convert C to F
 
 const updateCity = async city => {
   const cityDetails = await getCity(city);
